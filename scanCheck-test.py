d2e9deb30e19b1718577e08df1da2d1ebe2bf87b
gwainjgp <jgp2585@madrid.org>
2021-11-05 13:12:51 +0100

    x

---
+++ scanCheck-test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*
+#
+# Este script prueba las funciones de analisis que se vayan añadiendo
+#
+
+
+
+import argparse,json,io
+
+#Procesamos los argumentos
+parser = argparse.ArgumentParser(description='Probando las opciones de analisis')
+parser.add_argument('command',help='comando, list to get all posibilities', default='nothing')
+parser.add_argument('scanfile', help='Enrich scan file to analize')
+parser.add_argument('-r','--referencefile', help='Enrich scan file to compare')
+
+args = parser.parse_args()
+
+command = args.command
+command = command.lower()
+
+## Functions
+def loadScan(fichero):
+    with open(fichero) as savedDataFile:
+        try:
+            oldScanJson = {}
+            oldScanJson = json.load(savedDataFile)
+            savedDataFile.close()
+            return oldScanJson
+        except Exception as error:
+            print ('Error al procesar el fichero json: ',fichero, 'error: ', error)
+            
+            
+# Importamos lo que sabe hacer el cuñao
+import checklib
+from checklib import *
+checkActions = checklib.__all__
+
+if (command == 'list' ):
+    print ('Estos son los check se se pueden hacer:')
+    for i in checkActions:
+        print ('  *  Name: ',i, ', description: "',eval(i).getDescription(), '", re expresion: ',eval(i).getRE())
+
+else:
+    print ('# Se solicita probar el comando: ', command)
+    if (command in checkActions):
+        scanData = loadScan(args.scanfile)
+        print ('# Probando:' + command)
+        if eval(command).getReference():
+            referenceData = loadScan(args.referencefile)
+            if args.referencefile:
+                resultCommand = eval(command).getMade(scanData,referenceData)
+            else:
+                resultCommand = 'Este comando requiere fichero de referencia y no la ha indicado'              
+        else:
+            resultCommand = eval(command).getMade(scanData)
+        print ('# Resultado:')
+        print(resultCommand)
+
+    else:
+        print ('Error, no se conoce el comando: ' + command)
