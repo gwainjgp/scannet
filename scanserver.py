00162b12c21c14ff4e7d480ba1fd5557985a4294
Scannet <scannet@fw-watcher.madrid.org>
2021-12-09 15:58:52 +0100

    Add

--- scanserver.py
+++ scanserver.py
@@ -17,36 +17,8 @@ parser.add_argument('-c','--configfile', help='Cofiguración en formato JSON', d
 args = parser.parse_args()
 configFile = args.configfile
 
-# Aux functions
-
-
-def load_json(fichero):
-    with open(fichero) as File:
-        try:
-            config = {}
-            config = json.load(File)
-            File.close()
-            return config
-        except Exception as error:
-            print ('Error al procesar el fichero json: ',fichero, 'error: ', error)
-
-class config:
-    configFile = configFile
-    def __init__(self):
-        self.config = load_json(self.configFile)
-        self.config['default'] = self.config['ipNetworks'][0]
-
-    def getRoute(self):
-        return self.config['directory'] + '/' + self.config['default'] + '/' + self.config['filePattern']
-
-    def getScanFile(self):
-        list_of_files = glob.glob(self.getRoute()) # * menns all if need specific format then *.csv
-        list_of_files.sort(key=os.path.getctime)
-        return list_of_files[-1]
-    def getReferenceFile(self):
-        list_of_files = glob.glob(self.getRoute()) # * means all if need specific format then *.csv
-        list_of_files.sort(key=os.path.getctime)
-        return list_of_files[-2]
+# Load config
+from config import * # cargo config como absoluto
 
 ## Funciones de análisis
 # Importamos lo que sabemos hacer
